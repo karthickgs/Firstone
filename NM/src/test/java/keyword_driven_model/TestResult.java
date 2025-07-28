732499276f8f1b6693910af8c55b363497b4180b
k1027 <k1027@SGIVDI-0079.SVSGLOBAL.COM>
2025-07-28 19:01:35 +0530

    runnable class implemented for key word data driven

--- NM/src/test/java/keyword_driven_model/TestResult.java
+++ NM/src/test/java/keyword_driven_model/TestResult.java
@@ -1,5 +1,15 @@
 package keyword_driven_model;
 
-public class TestResult {
+import org.openqa.selenium.TakesScreenshot;
+import org.testng.ITestResult;
 
+public class TestResult implements ITestResult{
+
+	public void onTestFailure(ITestResult result) {
+		
+			String failed_TC = result.getName();
+			TakesScreenshot ss = (driver)TakeScreenshot;
+		}
+		
+	}
 }
